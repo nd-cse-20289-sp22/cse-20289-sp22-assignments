59b5b5b893bcf15f3dddc9948b070828d53a5a20
Peter Bui <pbui@nd.edu>
2022-01-21 12:01:52 -0500

    Reading 03: add filters template

---
+++ reading03/filters.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+q1_answer() {
+    # TODO: Complete pipeline
+    echo "Read the fine manual" | ...
+}
+
+q2_answer() {
+    # TODO: Complete pipeline
+    echo "BTW, I use Arch" | ...
+}
+
+q3_answer() {
+    # TODO: Complete pipeline
+    echo "     Yes, do as I say" | ...
+}
+
+q4_answer() {
+    # TODO: Complete pipeline
+    curl -sL https://yld.me/raw/yWh | ...
+}
+
+q5_answer() {
+    # TODO: Complete pipeline
+    curl -sL https://yld.me/raw/yWh | ...
+}
+
+q6_answer() {
+    # TODO: Complete pipeline
+    curl -sL https://yld.me/raw/yWh | ...
+}
