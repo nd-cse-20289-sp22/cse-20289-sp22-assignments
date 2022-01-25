368d0c165aa151c5c5b1580efb6c13737aa48042
Peter Bui <pbui@nd.edu>
2022-01-25 15:29:40 -0500

    Reading 04: update template

--- reading04/filters.sh
+++ reading04/filters.sh
@@ -2,32 +2,40 @@
 
 q1_answer() {
     # TODO: List only the names of the turtles in sorted order.
+    curl -sL https://yld.me/raw/hoUY | ...
 }
 
 q2_answer() {
     # TODO: List only the colors of the turtles in all capitals.
+    curl -sL https://yld.me/raw/hoUY | ...
 }
 
 q3_answer() {
     # TODO: Replace all weapons with plowshares
+    curl -sL https://yld.me/raw/hoUY | ...
 }
 
 q4_answer() {
     # TODO: List only the turtles whose names end in lo.
+    curl -sL https://yld.me/raw/hoUY | ...
 }
 
 q5_answer() {
     # TODO: List only the turtles with names that have two consecutive vowels.
+    curl -sL https://yld.me/raw/hoUY | ...
 }
 
 q6_answer() {
     # TODO: Count how many colors don't begin with a vowel
+    curl -sL https://yld.me/raw/hoUY | ...
 }
 
 q7_answer() {
     # TODO: List only the turtles names whose name ends with a vowel and whose weapon ends with a vowel.
+    curl -sL https://yld.me/raw/hoUY | ...
 }
 
 q8_answer() {
     # TODO: List only the turtles names with two of the same consecutive letter (i.e. aa, bb, etc.)
+    curl -sL https://yld.me/raw/hoUY | ...
 }
