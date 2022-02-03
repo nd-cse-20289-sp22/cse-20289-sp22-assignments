93b6e0cb3dff298d2c9f403950480461fe7e854a
Peter Bui <pbui@nd.edu>
2022-02-03 14:26:32 -0500

    Reading 05: add test_exists.sh

---
+++ reading05/test_exists.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+SCRIPT=exists.py
+WORKSPACE=/tmp/$SCRIPT.$(id -u)
+FAILURES=0
+
+error() {
+    echo "$@"
+    echo
+    [ -r $WORKSPACE/test ] && cat $WORKSPACE/test
+    echo
+    FAILURES=$((FAILURES + 1))
+}
+
+cleanup() {
+    STATUS=${1:-$FAILURES}
+    rm -fr $WORKSPACE
+    exit $STATUS
+}
+
+mkdir $WORKSPACE
+
+trap "cleanup" EXIT
+trap "cleanup 1" INT TERM
+
+echo "Testing $SCRIPT ..."
+
+printf " %-40s ... " "exists.py"
+./$SCRIPT * > /dev/null
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "exists.py *"
+./$SCRIPT * > /dev/null
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "exists.py * ASDF"
+./$SCRIPT * ASDF > /dev/null
+if [ $? -ne 1 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "exists.py /etc/*"
+./$SCRIPT /etc/* > /dev/null
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+TESTS=$(($(grep -c Success $0) - 1))
+echo "   Score $(echo "scale=2; ($TESTS - $FAILURES) / $TESTS.0 * 1.0" | bc | awk '{printf "%0.2f\n", $1}') / 1.00"
+printf "  Status "
+if [ $FAILURES -eq 0 ]; then
+    echo "Success"
+else
+    echo "Failure"
+
+fi
+echo
