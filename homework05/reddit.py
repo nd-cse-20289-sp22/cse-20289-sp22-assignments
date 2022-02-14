279b4d206bc4430070d553fa1bf4c542581c6681
Peter Bui <pbui@nd.edu>
2022-02-14 15:57:34 -0500

    Homework 05: add reddit template

---
+++ homework05/reddit.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import requests
+
+# Constants
+
+ISGD_URL = 'http://is.gd/create.php'
+
+# Functions
+
+def usage(status=0):
+    ''' Display usage information and exit with specified status '''
+    print('''Usage: {} [options] URL_OR_SUBREDDIT
+
+    -s          Shorten URLs using (default: False)
+    -n LIMIT    Number of articles to display (default: 10)
+    -o ORDERBY  Field to sort articles by (default: score)
+    -t TITLELEN Truncate title to specified length (default: 60)
+    '''.format(os.path.basename(sys.argv[0])))
+    sys.exit(status)
+
+def load_reddit_data(url):
+    ''' Load reddit data from specified URL into dictionary
+
+    >>> len(load_reddit_data('https://reddit.com/r/nba/.json'))
+    27
+
+    >>> load_reddit_data('linux')[0]['data']['subreddit']
+    'linux'
+    '''
+    # TODO: Verify url parameter (if it starts with http, then use it,
+    # otherwise assume it is just a subreddit).
+    return []
+
+def shorten_url(url):
+    ''' Shorten URL using is.gd service
+
+    >>> shorten_url('https://reddit.com/r/aoe2')
+    'https://is.gd/dL5bBZ'
+
+    >>> shorten_url('https://cse.nd.edu')
+    'https://is.gd/3gwUc8'
+    '''
+    # TODO: Make request to is.gd service to generate shortened url.
+    return ''
+
+def print_reddit_data(data, limit=10, orderby='score', titlelen=60, shorten=False):
+    ''' Dump reddit data based on specified attributes '''
+    # TODO: Sort articles stored in data list by the orderby key, and then
+    # print out each article's index, title, score, and url using the following
+    # format:
+    #
+    #   print(f'{index:4}.\t{title} (Score: {score})\n\t{url}')
+    #
+    # Note: Trim or modify the output based on the keyword arguments to the function.
+    pass
+
+def main():
+    # TODO: Parse command line arguments
+    arguments = sys.argv[1:]
+    url       = None
+    limit     = 10
+    orderby   = 'score'
+    titlelen  = 60
+    shorten   = False
+
+    # TODO: Load data from url and then print the data
+    pass
+
+# Main Execution
+
+if __name__ == '__main__':
+    main()
+
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
