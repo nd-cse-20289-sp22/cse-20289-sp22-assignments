8169b1fe578ab62d11bbbdd08f952985cb75141c
Peter Bui <pbui@nd.edu>
2022-03-10 15:12:06 -0500

    Reading 05: increase precision

--- reading05/test_exists.sh
+++ reading05/test_exists.sh
@@ -58,7 +58,7 @@ else
 fi
 
 TESTS=$(($(grep -c Success $0) - 2))
-echo "   Score $(echo "scale=2; ($TESTS - $FAILURES) / $TESTS.0 * 1.0" | bc | awk '{printf "%0.2f\n", $1}') / 1.00"
+echo "   Score $(echo "scale=4; ($TESTS - $FAILURES) / $TESTS.0 * 1.0" | bc | awk '{printf "%0.2f\n", $1}') / 1.00"
 printf "  Status "
 if [ $FAILURES -eq 0 ]; then
     echo "Success"
