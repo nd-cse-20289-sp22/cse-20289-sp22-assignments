b838aa2167953eb9d76b2c69919bfb6023ab19db
Peter Bui <pbui@nd.edu>
2022-03-10 15:12:47 -0500

    Reading 07: increase precision

--- reading07/test_evens.sh
+++ reading07/test_evens.sh
@@ -87,7 +87,7 @@ fi
 TESTS=$(($(grep -c Success $0) - 2))
 
 echo
-echo "   Score $(echo "scale=2; ($TESTS - $FAILURES) / $TESTS.0 * 1.0" | bc) / 1.00"
+echo "   Score $(echo "scale=4; ($TESTS - $FAILURES) / $TESTS.0 * 1.0" | bc | awk '{printf "%0.2f\n", $1}') / 1.00"
 printf "  Status "
 if [ $FAILURES -eq 0 ]; then
     echo "Success"
