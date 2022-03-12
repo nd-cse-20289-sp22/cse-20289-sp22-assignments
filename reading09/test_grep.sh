308701c390f9e835a482a6d9a10ffb1ef7a1b8ba
Peter Bui <pbui@nd.edu>
2022-03-12 15:02:14 -0500

    Reading 09: add test scripts

---
+++ reading09/test_grep.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+WORKSPACE=/tmp/grep.$(id -u)
+FAILURES=0
+
+error() {
+    echo "$@"
+    [ -r $WORKSPACE/test ] && (echo; cat $WORKSPACE/test; echo)
+    FAILURES=$((FAILURES + 1))
+}
+
+cleanup() {
+    STATUS=${1:-$FAILURES}
+    rm -fr $WORKSPACE
+    exit $STATUS
+}
+
+mkdir $WORKSPACE
+
+trap "cleanup" EXIT
+trap "cleanup 1" INT TERM
+
+echo
+echo "Checking reading09 grep ..."
+
+printf " %-40s ... " "grep usage (-h)"
+if ! ./grep -h |& grep -q -i usage; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "grep usage (no arguments)"
+if ! ./grep |& grep -q -i usage; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "grep root /etc/passwd"
+valgrind --leak-check=full ./grep root < /etc/passwd &> $WORKSPACE/test
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "grep root /etc/passwd (valgrind)"
+if [ "$(awk '/ERROR SUMMARY/ {print $4}' $WORKSPACE/test)" -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "grep login /etc/passwd"
+valgrind --leak-check=full ./grep login < /etc/passwd &> $WORKSPACE/test
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "grep login /etc/passwd (valgrind)"
+if [ "$(awk '/ERROR SUMMARY/ {print $4}' $WORKSPACE/test)" -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "grep asdf /etc/passwd"
+valgrind --leak-check=full ./grep asdf < /etc/passwd &> $WORKSPACE/test
+if [ $? -eq 0 ]; then
+    error "Failure (Wrong exit code)"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "grep asdf /etc/passwd (valgrind)"
+if [ "$(awk '/ERROR SUMMARY/ {print $4}' $WORKSPACE/test)" -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+TESTS=$(($(grep -c Success $0) - 2))
+echo
+echo "   Score $(echo "scale=4; ($TESTS - $FAILURES) / $TESTS.0 * 1.0" | bc | awk '{printf "%0.2f\n", $1}') / 1.00"
+printf "  Status "
+if [ $FAILURES -eq 0 ]; then
+    echo "Success"
+else
+    echo "Failure"
+fi
+echo
---
+++ reading09/test_sort.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+WORKSPACE=/tmp/sort.$(id -u)
+FAILURES=0
+
+error() {
+    echo "$@"
+    [ -r $WORKSPACE/test ] && (echo; cat $WORKSPACE/test; echo)
+    FAILURES=$((FAILURES + 1))
+}
+
+cleanup() {
+    STATUS=${1:-$FAILURES}
+    rm -fr $WORKSPACE
+    exit $STATUS
+}
+
+mkdir $WORKSPACE
+
+trap "cleanup" EXIT
+trap "cleanup 1" INT TERM
+
+echo "Checking reading 09 sort ..."
+
+printf " %-40s ... " "sort 1"
+shuf -i 1-1 | ./sort | diff -y - <(seq 1) > $WORKSPACE/test
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "sort 1 10"
+shuf -i 1-10 | ./sort | diff -y - <(seq 1 10) > $WORKSPACE/test
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "sort 1 100"
+shuf -i 1-100 | ./sort | diff -y - <(seq 1 100) > $WORKSPACE/test
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "sort 1 1000"
+shuf -i 1-1000 | ./sort | diff -y - <(seq 1 1000) > $WORKSPACE/test
+if [ $? -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+printf " %-40s ... " "sort 1 1000 (valgrind)"
+shuf -i 1-1000 | valgrind --leak-check=full ./sort &> $WORKSPACE/test
+if [ $? -ne 0 ] || [ "$(awk '/ERROR SUMMARY/ {print $4}' $WORKSPACE/test)" -ne 0 ]; then
+    error "Failure"
+else
+    echo "Success"
+fi
+
+TESTS=$(($(grep -c Success $0) - 2))
+echo
+echo "   Score $(echo "scale=4; ($TESTS - $FAILURES) / $TESTS.0 * 1.0" | bc | awk '{printf "%0.2f\n", $1}') / 1.00"
+printf "  Status "
+if [ $FAILURES -eq 0 ]; then
+    echo "Success"
+else
+    echo "Failure"
+fi
+echo
